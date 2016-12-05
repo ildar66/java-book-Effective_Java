a80f414b3689f412c832debb0cba97399fc6df1a
ildar shafigullin <ildar66@inbox.ru>
2016-12-05 10:46:18 +0300

    Item 3: Enforce noninstantiability with a private constructor

---
+++ First_Edition/src/chapter_2/item_3/UtilityClass.java
@@ -0,0 +1,12 @@
+package chapter_2.item_3;
+
+// Noninstantiable utility class
+public class UtilityClass {
+
+    // Suppress default constructor for noninstantiability
+    private UtilityClass() {
+        // This constructor will never be invoked
+    }
+
+    //... // Remainder omitted
+}
---
+++ First_Edition/src/chapter_2/item_3/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by User on 05.12.2016.
+ * Item 3: Enforce noninstantiability with a private constructor
+ */
+package chapter_2.item_3;
