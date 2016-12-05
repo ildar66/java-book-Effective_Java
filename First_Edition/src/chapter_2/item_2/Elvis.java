fc5b94825ea097e2555ad51571ebb96ee70ee0de
ildar shafigullin <ildar66@inbox.ru>
2016-12-05 10:30:15 +0300

    Item 2: Enforce the singleton property with a private constructor.readResolve method to preserve singleton property

--- First_Edition/src/chapter_2/item_2/Elvis.java
+++ First_Edition/src/chapter_2/item_2/Elvis.java
@@ -1,5 +1,7 @@
 package chapter_2.item_2;
 
+import java.io.ObjectStreamException;
+
 // Singleton with final field
 public class Elvis {
 
@@ -9,4 +11,13 @@ public class Elvis {
         //...
     }
     //... // Remainder omitted
+
+    // readResolve method to preserve singleton property
+    private Object readResolve() throws ObjectStreamException {
+        /**
+         * Return the one true Elvis and let the garbage collector
+         * take care of the Elvis impersonator.
+         */
+        return INSTANCE;
+    }
 }
--- First_Edition/src/chapter_2/item_2/Elvis2.java
+++ First_Edition/src/chapter_2/item_2/Elvis2.java
@@ -1,5 +1,7 @@
 package chapter_2.item_2;
 
+import java.io.ObjectStreamException;
+
 // Singleton with static factory
 public class Elvis2 {
 
@@ -13,4 +15,13 @@ public class Elvis2 {
         return INSTANCE;
     }
     //... // Remainder omitted
+
+    // readResolve method to preserve singleton property
+    private Object readResolve() throws ObjectStreamException {
+        /**
+         * Return the one true Elvis and let the garbage collector
+         * take care of the Elvis impersonator.
+         */
+        return INSTANCE;
+    }
 }
