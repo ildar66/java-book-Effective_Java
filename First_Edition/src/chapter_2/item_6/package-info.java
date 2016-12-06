f28f303ebc595549b2e50dfa669b6ce67e6a57df
ildar shafigullin <ildar66@inbox.ru>
2016-12-06 10:13:42 +0300

    Item 6: Avoid finalizers. Finalizer Guardian idiom

--- First_Edition/src/chapter_2/item_6/package-info.java
+++ First_Edition/src/chapter_2/item_6/package-info.java
@@ -1,5 +1,32 @@
 /**
  * Created by User on 05.12.2016.
  * Item 6: Avoid finalizers
+ *
+ * nothing time-critical should ever be done by a finalizer.
+ *
+ * you should never depend on a finalizer to update critical persistent state.
+ *
+ * provide an explicit termination method.
+ *
+ * Explicit termination methods are often used in combination with the try-finally construct to ensure prompt termination.
+     // try-finally block guarantees execution of termination method
+     Foo foo = new Foo(...);
+     try {
+        // Do what must be done with foo
+        ...
+     } finally {
+        foo.terminate(); // Explicit termination method
+     }
+ *
+ *
+ *   // Manual finalizer chaining
+     protected void finalize() throws Throwable {
+         try {
+            // Finalize subclass state
+                ...
+         } finally {
+            super.finalize();
+         }
+     }
  */
 package chapter_2.item_6;
