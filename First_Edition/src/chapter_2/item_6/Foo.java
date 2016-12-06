50e439af6acb753d85aa48c097f5f033083e0c6d
ildar shafigullin <ildar66@inbox.ru>
2016-12-06 10:15:53 +0300

    Item 6: Avoid finalizers. Finalizer Guardian idiom

--- First_Edition/src/chapter_2/item_6/Foo.java
+++ First_Edition/src/chapter_2/item_6/Foo.java
@@ -1,6 +1,14 @@
 package chapter_2.item_6;
 
 // Finalizer Guardian idiom
+
+/**
+ * In summary, don't use finalizers except as a safety net or to terminate noncritical native
+ * resources. In those rare instances where you do use a finalizer, remember to invoke
+ * super.finalize. Last , if you need to associate a finalizer with a public, nonfinal class,
+ * consider using a finalizer guardian to ensure that the finalizer is executed, even if a subclass
+ * finalizer fails to invoke super.finalize.
+ */
 public class Foo {
 
     // Sole purpose of this object is to finalize outer Foo object
