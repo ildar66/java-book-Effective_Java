ed2a1e2b93cdc39725e00d372b638ec613cbc2ff
ildar shafigullin <ildar66@inbox.ru>
2016-12-06 10:54:29 +0300

    Item 7: Obey the general contract when overriding equals. Broken - violates transitivity.

--- First_Edition/src/chapter_3/item_7/ColorPoint.java
+++ First_Edition/src/chapter_3/item_7/ColorPoint.java
@@ -10,11 +10,23 @@ public class ColorPoint extends Point {
         super(x, y);
         this.color = color;
     }
-
+/*
     //Broken - violates symmetry!
     public boolean equals(Object o) {
         if (!(o instanceof ColorPoint))
             return false;
+        ColorPoint cp = (ColorPoint) o;
+        return super.equals(o) && cp.color == color;
+    }*/
+
+    //Broken - violates transitivity.
+    public boolean equals(Object o) {
+        if (!(o instanceof Point))
+            return false;
+        // If o is a normal Point, do a color-blind comparison
+        if (!(o instanceof ColorPoint))
+            return o.equals(this);
+        // o is a ColorPoint; do a full comparison
         ColorPoint cp = (ColorPoint) o;
         return super.equals(o) && cp.color == color;
     }
