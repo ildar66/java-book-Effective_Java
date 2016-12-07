1385d0a227144fba6ad144475efa73829100ddbb
ildar shafigullin <ildar66@inbox.ru>
2016-12-07 11:43:27 +0300

    Item 9: Always override toString.

--- First_Edition/src/chapter_3/item_9/package-info.java
+++ First_Edition/src/chapter_3/item_9/package-info.java
@@ -1,5 +1,8 @@
 /**
- * Created by User on 07.12.2016.
  * Item 9: Always override toString.
+ * Providing a good toString implementation makes your class much more pleasant to use.
+ * When practical, the toString method should return all of the interesting information contained in the object
+ * Whether or not you decide to specify the format, you should clearly document your intentions.
+ * It is always a good idea to provide programmatic access to all of the information contained in the value returned by toString
  */
 package chapter_3.item_9;
