2fcfc3da7b350cca413f21e3af6d34ce9696a174
ildar shafigullin <ildar66@inbox.ru>
2016-12-07 12:52:16 +0300

    Item 10: Override clone judiciously.HashTable example.

--- First_Edition/src/chapter_3/item_10/HashTable.java
+++ First_Edition/src/chapter_3/item_10/HashTable.java
@@ -22,14 +22,31 @@ public class HashTable implements Cloneable {
             this.value = value;
             this.next = next;
         }
+
+        // Recursively copy the linked list headed by this Entry
+        Entry deepCopy() {
+            return new Entry(key, value, next == null ? null : next.deepCopy());
+        }
     }
 
-    // Broken - results in shared internal state!
+    public Object clone() throws CloneNotSupportedException {
+        HashTable result = (HashTable) super.clone();
+        result.buckets = new Entry[buckets.length];
+        for (int i = 0; i < buckets.length; i++) {
+            if (buckets[i] != null) {
+                result.buckets[i] = (Entry) buckets[i].deepCopy();
+            }
+        }
+        return result;
+    }
+
+/*
+   // Broken - results in shared internal state!
     public Object clone() throws CloneNotSupportedException {
         HashTable result = (HashTable) super.clone();
         result.buckets = (Entry[]) buckets.clone();
         return result;
-    }
+    }*/
 
     // ... // Remainder omitted
 }
