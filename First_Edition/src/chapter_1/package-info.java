b5818beee39514bc8f68cc4c51387093223552b3
ildar shafigullin <ildar66@inbox.ru>
2016-12-08 10:50:35 +0300

    Comments to chapters 1,2,3,4

--- First_Edition/src/chapter_1/package-info.java
+++ First_Edition/src/chapter_1/package-info.java
@@ -1,5 +1,16 @@
 /**
- * Created by User on 03.12.2016.
  * Introduction chapter.
+ * This book is designed to help you make the most effective use of the Java™ programming
+ * language and its fundamental libraries, java.lang, java.util, and, to a lesser extent,
+ * java.io. The book discusses other libraries from time to time, but it does not cover graphical
+ * user interface programming or enterprise APIs.
+ * This book consists of fifty-seven items, each of which conveys one rule. The rules capture
+ * practices generally held to be beneficial by the best and most experienced programmers.
+ * The items are loosely grouped into nine chapters, each concerning one broad aspect of
+ * software design. The book is not intended to be read from cover to cover: Each item stands on
+ * its own, more or less. The items are heavily cross-referenced so you can easily plot your own
+ * course through the book.
+ * Most items are illustrated with program examples. A key feature of this book is that it
+ * contains code examples illustrating many design patterns and idioms.
  */
 package chapter_1;
