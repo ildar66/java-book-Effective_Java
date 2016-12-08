b8103bf16dfcc3165fb7ff19fef43db3c021e84d
ildar shafigullin <ildar66@inbox.ru>
2016-12-08 12:01:08 +0300

    Item 13: Favor immutability.Complex example.

---
+++ First_Edition/src/chapter_4/item_13/Complex.java
@@ -0,0 +1,67 @@
+package chapter_4.item_13;
+
+/**
+ * Many of the example classes in previous items are immutable. One such class is PhoneNumber
+ * in Item 8, which has accessors for each attribute but no corresponding mutators. Here is
+ * a slightly more complex example:
+ */
+public final class Complex {
+
+    private final float re;
+    private final float im;
+
+    public Complex(float re, float im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    // Accessors with no corresponding mutators
+    public float realPart() {
+        return re;
+    }
+
+    public float imaginaryPart() {
+        return im;
+    }
+
+    public Complex add(Complex c) {
+        return new Complex(re + c.re, im + c.im);
+    }
+
+    public Complex subtract(Complex c) {
+        return new Complex(re - c.re, im - c.im);
+    }
+
+    public Complex multiply(Complex c) {
+        return new Complex(re * c.re - im * c.im,
+                           re * c.im + im * c.re);
+    }
+
+    public Complex divide(Complex c) {
+        float tmp = c.re * c.re + c.im * c.im;
+        return new Complex((re * c.re + im * c.im) / tmp,
+                           (im * c.re - re * c.im) / tmp);
+    }
+
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Complex))
+            return false;
+        Complex c = (Complex) o;
+        return (Float.floatToIntBits(re) == // See page 33 to
+                Float.floatToIntBits(c.re)) && // find out why
+                (Float.floatToIntBits(im) == // floatToIntBits
+                        Float.floatToIntBits(im)); // is used.
+    }
+
+    public int hashCode() {
+        int result = 17 + Float.floatToIntBits(re);
+        result = 37 * result + Float.floatToIntBits(im);
+        return result;
+    }
+
+    public String toString() {
+        return "(" + re + " + " + im + "i)";
+    }
+}
