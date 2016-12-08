a6e3bde71657928f929947b535516af995391cb7
ildar shafigullin <ildar66@inbox.ru>
2016-12-08 10:49:03 +0300

    Comments to chapters 1,2,3

--- First_Edition/src/chapter_2/package-info.java
+++ First_Edition/src/chapter_2/package-info.java
@@ -1,5 +1,7 @@
 /**
- * Created by User on 03.12.2016.
  * Creating and Destroying Objects chapter.
+ * This chapter concerns creating and destroying objects: when and how to create objects, when
+ * and how to avoid creating them, how to ensure that objects are destroyed in a timely manner,
+ * and how to manage any cleanup actions that must precede object destruction.
  */
 package chapter_2;
