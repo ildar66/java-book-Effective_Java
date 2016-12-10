51dbf8e95e60d3dc6db951fb64010b169384d297
ildar shafigullin <ildar66@inbox.ru>
2016-12-10 11:52:20 +0300

    Item 16: Prefer interfaces to abstract classes.Summary.

--- First_Edition/src/chapter_4/item_16/package-info.java
+++ First_Edition/src/chapter_4/item_16/package-info.java
@@ -11,6 +11,13 @@
  *
  * You can combine the virtues of interfaces and abstract classes by providing an abstract skeletal implementation class
  * to go with each nontrivial interface that you export.
+ *
+ * To summarize, an interface is generally the best way to define a type that permits multiple implementations.
+ * An exception to this rule is the case where ease of evolution is deemed more important than flexibility and power.
+ * Under these circumstances, you should use an abstract class to define the type, but only if you understand and can accept the limitations.
+ * If you export a nontrivial interface, you should strongly consider providing a skeletal implementation to go with it.
+ * Finally, you should design all of your public interfaces with the utmost care
+ * and test them thoroughly by writing multiple implementations.
  */
 /*
 // List adapter for int array
