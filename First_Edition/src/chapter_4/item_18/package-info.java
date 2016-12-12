3b3f77753734b1fb3a0f5988c15f69d552d03a30
ildar shafigullin <ildar66@inbox.ru>
2016-12-12 11:00:23 +0300

    Item 18: Favor static member classes over nonstatic.Summary

--- First_Edition/src/chapter_4/item_18/package-info.java
+++ First_Edition/src/chapter_4/item_18/package-info.java
@@ -11,7 +11,7 @@
  *      All but the first kind are known as inner classes.
  *
  * One common use of a static member class is as a public auxiliary class, useful only in conjunction with its outer class.
- * For example, consider a typesafe enum describing the operations supported by a calculator (Item 21).
+ * For example, consider a typesafe enum describing the operations supported by a @see {@link chapter_4.item_18.Calculator}.
  * The Operation class should be a public static member class of the Calculator class. Clients of the Calculator class could then refer to
  * operations using names like Calculator.Operation.PLUS and Calculator.Operation.MINUS. This use is demonstrated later in this item.
  *
@@ -20,5 +20,28 @@
  * to implement their collection views, which are returned by Map's keySet, entrySet, and values methods. @see {@link chapter_4.item_18.MySet}
  *
  * If you declare a member class that does not require access to an enclosing instance, remember to put the static modifier in the declaration.
+ *
+ * Anonymous classes are unlike anything else in the Java programming language. As you would expect, an anonymous class has no name.
+ * It is not a member of its enclosing class. Rather than being declared along with other members, it is simultaneously declared and
+ * instantiated at the point of use. Anonymous classes are permitted at any point in the code where an expression is legal.
+ * Anonymous classes behave like static or nonstatic member classes depending on where they occur:
+ * They have enclosing instances if they occur in a nonstatic context.
+ * // Typical use of an anonymous class
+     Arrays.sort(args, new Comparator() {
+        public int compare(Object o1, Object o2) {
+            return ((String)o1).length() - ((String)o2).length();
+     }
+     });
+ * Another common use of an anonymous class is to create a process object, such as a Thread, Runnable, or TimerTask instance.
+ * A third common use is within a static factory method (see the intArrayAsList method in Item 16).
+ * A fourth common use is in the public static final field initializers of sophisticated typesafe enums that require a separate subclass
+ * for each instance @see {@link chapter_4.item_18.Calculator}.
+ */
+/*
+    To recap, there are four different kinds of nested classes, and each has its place. If a nested class needs to be visible outside of a
+    single method or is too long to fit comfortably inside a method, use a member class.
+    If each instance of the member class needs a reference to its enclosing instance, make it nonstatic; otherwise make it static.
+    Assuming the class belongs inside a method, if you need to create instances from only one location and there is a
+    preexisting type that characterizes the class, make it an anonymous class; otherwise, make it a local class.
  */
 package chapter_4.item_18;
