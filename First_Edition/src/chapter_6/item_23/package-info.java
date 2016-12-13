6234b87f792bde978bd00a694b099b4454d4922c
ildar shafigullin <ildar66@inbox.ru>
2016-12-13 12:04:02 +0300

    Item 23: Check parameters for validity

---
+++ First_Edition/src/chapter_6/item_23/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Item 23: Check parameters for validity.
+ */
+package chapter_6.item_23;
---
+++ First_Edition/src/chapter_6/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Chapter 6. Methods.
+ * This chapter discusses several aspects of method design: how to treat parameters and return values, how to design method signatures,
+ * and how to document methods. Much of the material in this chapter applies to constructors as well as to methods.
+ * Like Chapter 5, this chapter focuses on usability, robustness, and flexibility.
+ */
+package chapter_6;
