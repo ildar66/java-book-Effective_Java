0698752becc690dfc3d66f866198dfba14b8442b
ildar shafigullin <ildar66@inbox.ru>
2016-12-13 10:59:21 +0300

    Item 21: Replace enum constructs with classes

--- First_Edition/src/chapter_5/item_21/package-info.java
+++ First_Edition/src/chapter_5/item_21/package-info.java
@@ -1,19 +1,4 @@
 /**
  * Item 21: Replace enum constructs with classes.
- * 
- * Because typesafe enum constants are objects, you can put them into collections. For example,
- * suppose you want the Suit class to export an immutable list of the suits in standard order.
- * Merely add these two field declarations to the class:
- * private static final Suit[] PRIVATE_VALUES = { CLUBS, DIAMONDS, HEARTS, SPADES };
- * public static final List VALUES = Collections.unmodifiableList(Arrays.asList(PRIVATE_VALUES));
- *
- * Unlike the simplest form of the typesafe enum pattern, classes of the ordinal-based form
- * above can be made serializable (Chapter 10) with a little care. It is not sufficient merely to
- * add implements Serializable to the class declaration.
- *
- * You must also provide a readResolve method (Item 57):
- * private Object readResolve() throws ObjectStreamException {
- *      return PRIVATE_VALUES[ordinal]; // Canonicalize
- * }
  */
 package chapter_5.item_21;
