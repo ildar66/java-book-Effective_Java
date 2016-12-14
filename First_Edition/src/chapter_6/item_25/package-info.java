6fc587711d9b97a8ee37f97e7f07cf9de1294767
ildar shafigullin <ildar66@inbox.ru>
2016-12-14 11:29:03 +0300

    Item 25: Design method signatures carefully.

---
+++ First_Edition/src/chapter_6/item_25/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Item 25: Design method signatures carefully.
+ *
+ * Choose method names carefully.
+ *
+ * Don't go overboard in providing convenience methods.
+ *
+ * Avoid long parameter lists.Long sequences of identically typed parameters are especially harmful.
+ *
+ * For parameter types, favor interfaces over classes.
+ *
+ * Use function objects (Item 22) judiciously.
+ */
+package chapter_6.item_25;
