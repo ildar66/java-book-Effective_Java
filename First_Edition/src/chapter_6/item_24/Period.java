1274630b04c4ec5231fb810f97f1b3170b81a293
ildar shafigullin <ildar66@inbox.ru>
2016-12-14 11:08:44 +0300

    Item 24: Make defensive copies when needed. Broken "immutable" time period class

---
+++ First_Edition/src/chapter_6/item_24/Period.java
@@ -0,0 +1,31 @@
+package chapter_6.item_24;
+
+// Broken "immutable" time period class
+public final class Period {
+
+    private final Date start;
+    private final Date end;
+
+    /**
+     * @param start the beginning of the period.
+     * @param end the end of the period; must not precede start.
+     * @throws IllegalArgumentException if start is after end.
+     * @throws NullPointerException if start or end is null.
+     */
+    public Period(Date start, Date end) {
+        if (start.compareTo(end) > 0)
+            throw new IllegalArgumentException(start + " after " + end);
+        this.start = start;
+        this.end = end;
+    }
+
+    public Date start() {
+        return start;
+    }
+
+    public Date end() {
+        return end;
+    }
+
+    // ... // Remainder omitted
+}
---
+++ First_Edition/src/chapter_6/item_24/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Item 24: Make defensive copies when needed.
+ * You must program defensively with the assumption that clients of your class will do their best to destroy its invariants.
+ */
+package chapter_6.item_24;
