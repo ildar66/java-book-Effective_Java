c5aea4bf7ffd054be5a437154fb3f1e8ef31eb23
ildar shafigullin <ildar66@inbox.ru>
2016-12-14 11:15:39 +0300

    Item 24: Make defensive copies when needed. Repaired period class

--- First_Edition/src/chapter_6/item_24/Period.java
+++ First_Edition/src/chapter_6/item_24/Period.java
@@ -1,5 +1,7 @@
 package chapter_6.item_24;
 
+import java.util.Date;
+
 // Broken "immutable" time period class
 public final class Period {
 
@@ -19,6 +21,22 @@ public final class Period {
         this.end = end;
     }
 
+    // ... // Remainder omitted
+
+    // Attack the internals of a Period instance
+    //    Date start = new Date();
+    //    Date end = new Date();
+    //    Period p = new Period(start, end);
+    //    end.setYear(78); // Modifies internals of p!
+
+/*    // Repaired constructor - makes defensive copies of parameters
+    public Period(Date start, Date end) {
+        this.start = new Date(start.getTime());
+        this.end = new Date(end.getTime());
+        if (this.start.compareTo(this.end) > 0)
+            throw new IllegalArgumentException(start +" after "+ end);
+    }*/
+
     public Date start() {
         return start;
     }
@@ -27,5 +45,18 @@ public final class Period {
         return end;
     }
 
-    // ... // Remainder omitted
+    // Second attack on the internals of a Period instance
+    //    Date start = new Date();
+    //    Date end = new Date();
+    //    Period p = new Period(start, end);
+    //    p.end().setYear(78); // Modifies internals of p!
+
+/*    To defend against the second attack, merely modify the accessors to return defensive copies of mutable internal fields:
+    // Repaired accessors - make defensive copies of internal fields
+    public Date start() {
+        return (Date) start.clone();
+    }
+    public Date end() {
+        return (Date) end.clone();
+    }*/
 }
--- First_Edition/src/chapter_6/item_24/package-info.java
+++ First_Edition/src/chapter_6/item_24/package-info.java
@@ -1,5 +1,11 @@
 /**
  * Item 24: Make defensive copies when needed.
+ *
  * You must program defensively with the assumption that clients of your class will do their best to destroy its invariants.
+ *
+ * Defensive copies are made before checking the validity of the parameters (Item 23),
+ * and the validity check is performed on the copies rather than on the originals.
+ *
+ * Do not use the clone method to make a defensive copy of a parameter whose type is subclassable by untrusted parties.
  */
 package chapter_6.item_24;
