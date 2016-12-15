9230bd3026f9f40e9b803943825872560e980d89
ildar shafigullin <ildar66@inbox.ru>
2016-12-15 10:49:26 +0300

    Item 27: Return zero-length arrays, not nulls.

---
+++ First_Edition/src/chapter_6/item_27/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Item 27: Return zero-length arrays, not nulls.
+ * In summary, there is no reason ever to return null from an array-valued method instead of returning a zero-length array.
+ */
+//private List cheesesInStock = ...;
+//private final static Cheese[] NULL_CHEESE_ARRAY = new Cheese[0];
+/**
+ * @return an array containing all of the cheeses in the shop.
+ */
+//public Cheese[] getCheeses() {
+//   return (Cheese[]) cheesesInStock.toArray(NULL_CHEESE_ARRAY);
+//}
+package chapter_6.item_27;
