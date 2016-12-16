ada3ef6a671634919162b07ec99a55989417aea8
ildar shafigullin <ildar66@inbox.ru>
2016-12-16 11:12:31 +0300

    Item 32: Avoid strings where other types are more appropriate.

---
+++ First_Edition/src/chapter_7/item_32/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Item 32: Avoid strings where other types are more appropriate.
+ * Strings are poor substitutes for other value types.
+ * Strings are poor substitutes for enumerated types.
+ * Strings are poor substitutes for aggregate types.
+ * // Inappropriate use of string as aggregate type: String compoundKey = className + "#" + i.next();
+ * Strings are poor substitutes for capabilities.
+ */
+package chapter_7.item_32;
