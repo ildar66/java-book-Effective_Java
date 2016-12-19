8833bbfea4f198213eae3d4746909c54264e696a
ildar shafigullin <ildar66@inbox.ru>
2016-12-19 11:00:50 +0300

    Item 38: Adhere to generally accepted naming conventions.

---
+++ First_Edition/src/chapter_7/item_38/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * Item 38: Adhere to generally accepted naming conventions.
+ *
+ * Table 7.1. : Examples of Typographical Conventions
+ * Identifier Type Examples
+ * Package: com.sun.medialib, com.sun.jdi.event
+ * Class or Interface: Timer, TimerTask, KeyFactorySpi, HttpServlet
+ * Method or Field: remove, ensureCapacity, getCrc
+ * Constant: Field VALUES, NEGATIVE_INFINITY
+ * Local Variable: i, xref, houseNumber
+ *
+ * To summarize, internalize the standard naming conventions and learn to use them as second
+ * nature. The typographical conventions are straightforward and largely unambiguous;
+ * the grammatical conventions are more complex and looser. To quote from The Java
+ * Language Specification [JLS, 6.8], “These conventions should not be followed slavishly if
+ * long-held conventional usage dictates otherwise.” Use common sense.
+ */
+package chapter_7.item_38;
