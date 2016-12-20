00d70f4024ed069e4199761568ed22875f64f5ca
ildar shafigullin <ildar66@inbox.ru>
2016-12-20 11:17:11 +0300

    Item 43: Throw exceptions appropriate to the abstraction

---
+++ First_Edition/src/chapter_8/item_43/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Item 43: Throw exceptions appropriate to the abstraction.
+ * Higher layers should catch lower-level exceptions and, in their place,
+ * throw exceptions that are explainable in terms of the higher-level abstraction.
+ * While exception translation is superior to mindless propagation of exceptions from lower layers, it should not be overused.
+ */
+package chapter_8.item_43;
