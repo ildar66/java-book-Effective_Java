3e6d321c693ad42a0f4aea1d6d64f2337855a973
ildar shafigullin <ildar66@inbox.ru>
2016-12-20 11:05:45 +0300

    Item 42: Favor the use of standard exceptions.

---
+++ First_Edition/src/chapter_8/item_42/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Item 42: Favor the use of standard exceptions.
+ * *
+ * Table 8.1. Commonly Used Exceptions
+ * *
+ * Exception: Occasion for Use
+ * *
+ * IllegalArgumentException: Parameter value is inappropriate
+ * IllegalStateException: Object state is inappropriate for method invocation
+ * NullPointerException: Parameter value is null where prohibited
+ * IndexOutOfBoundsException: Index parameter value is out of range
+ * ConcurrentModificationException: Concurrent modification of object has been detected where prohibited
+ * UnsupportedOperationException: Object does not support method
+ */
+package chapter_8.item_42;
