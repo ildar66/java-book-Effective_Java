140317b8a71315d62f4a28cbc39fb27a294243d5
ildar shafigullin <ildar66@inbox.ru>
2016-12-20 10:11:22 +0300

    Item 39:Use exceptions only for exceptional conditions.

--- First_Edition/src/chapter_8/item_39/package-info.java
+++ First_Edition/src/chapter_8/item_39/package-info.java
@@ -1,4 +1,6 @@
 /**
  * Item 39:Use exceptions only for exceptional conditions.
+ * Exceptions are, as their name implies, to be used only for exceptional conditions; they should never be used for ordinary control flow.
+ * A well-designed API must not force its client to use exceptions for ordinary control flow.
  */
 package chapter_8.item_39;
