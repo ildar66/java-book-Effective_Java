7c83aa82c2d3163d241b857a38f1cf5ca1b8f7d8
ildar shafigullin <ildar66@inbox.ru>
2016-12-20 12:11:26 +0300

    Introduction Chapter.

---
+++ Second_Edition/src/chapter01/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * The following table shows you where to go for primary coverage of these features:
+ * Feature:          Chapter or Item:
+ * Generics:                Chapter 5
+ * Enums:                   Items 30–34
+ * Annotations:             Items 35–37
+ * For-each loop:           Item 46
+ * Autoboxing:              Items 40, 49
+ * Varargs:                 Item 42
+ * Static import:           Item 19
+ * java.util.concurrent:    Items 68, 69.
+ */
+package chapter01;
