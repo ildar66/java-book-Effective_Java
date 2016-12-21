55b6954459fe499b3ca1b88836e3dad5db7a9b97
ildar shafigullin <ildar66@inbox.ru>
2016-12-21 12:13:37 +0300

    Item 2: Consider a builder when faced with many constructor parameters. Telescoping constructor pattern.

---
+++ Second_Edition/src/chapter02/item02/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Item 2: Consider a builder when faced with many constructor parameters.
+ * *
+ * The telescoping constructor pattern works see {@link chapter02.item02.telescopingconstructor.NutritionFacts},
+ * but it is hard to write client code when there are many parameters, and harder still to read it.
+ * *
+ */
+package chapter02.item02;
---
+++ Second_Edition/src/chapter02/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Creating and Destroying Objects.
+ * *
+ * THIS chapter concerns creating and destroying objects:
+ * when and how to create them,
+ * when and how to avoid creating them,
+ * how to ensure they are destroyed in a timely manner,
+ * and how to manage any cleanup actions that must precede their destruction.
+ */
+package chapter02;
