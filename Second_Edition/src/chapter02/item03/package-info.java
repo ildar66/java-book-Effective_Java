a00804485c2c3999d519a269142af38c4fd6d516
ildar shafigullin <ildar66@inbox.ru>
2016-12-21 12:58:36 +0300

    Item 3: Enforce the singleton property with a private constructor or an enum type. Singleton with static factory.

--- Second_Edition/src/chapter02/item03/package-info.java
+++ Second_Edition/src/chapter02/item03/package-info.java
@@ -1,4 +1,8 @@
 /**
  * Item 3: Enforce the singleton property with a private constructor or an enum type.
+ * *
+ * Before release 1.5, there were two ways to implement singletons:
+ * In one approach, the member is a final field: see {@link chapter02.item03.field.Elvis}
+ * In the second approach to implementing singletons, the public member is a static factory method: see {@link chapter02.item03.method.Elvis}
  */
 package chapter02.item03;
