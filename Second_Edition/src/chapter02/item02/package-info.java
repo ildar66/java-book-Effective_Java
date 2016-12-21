4a96c4e87b76851d3ab269a0bb36e79567d925be
ildar shafigullin <ildar66@inbox.ru>
2016-12-21 12:20:33 +0300

    Item 2: Consider a builder when faced with many constructor parameters. JavaBeans pattern.

--- Second_Edition/src/chapter02/item02/package-info.java
+++ Second_Edition/src/chapter02/item02/package-info.java
@@ -4,5 +4,9 @@
  * The telescoping constructor pattern works see {@link chapter02.item02.telescopingconstructor.NutritionFacts},
  * but it is hard to write client code when there are many parameters, and harder still to read it.
  * *
+ * JavaBeans pattern has serious disadvantages of its own see {@link chapter02.item02.javabeans.NutritionFacts}.
+ * Because construction is split across multiple calls, a JavaBean may be in an inconsistent state partway through its construction.
+ * A related disadvantage is that the JavaBeans pattern precludes the possibility of making a class immutable.
+ * *
  */
 package chapter02.item02;
