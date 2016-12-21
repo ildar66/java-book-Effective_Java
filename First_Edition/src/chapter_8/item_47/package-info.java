85471cf45b93aa42a3e3cd3ae0c172d853a1ff47
ildar shafigullin <ildar66@inbox.ru>
2016-12-21 11:06:31 +0300

    Item 47:Don't ignore exceptions

---
+++ First_Edition/src/chapter_8/item_47/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Item 47:Don't ignore exceptions.
+ * *
+ * An empty catch block defeats the purpose of exceptions.
+ * At the very least, the catch block should contain a comment explaining why it is appropriate to ignore the exception.
+ * *
+ */
+package chapter_8.item_47;
