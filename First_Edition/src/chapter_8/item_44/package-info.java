5e36bb6628afa90a5daecac95d409d6a75389ab2
ildar shafigullin <ildar66@inbox.ru>
2016-12-21 10:50:25 +0300

    Item 44:Document all exceptions thrown by each method

---
+++ First_Edition/src/chapter_8/item_44/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Item 44:Document all exceptions thrown by each method.
+ * *
+ * Always declare checked exceptions individually,
+ * and document precisely the conditions under which each one is thrown using the Javadoc @throws tag.
+ * *
+ * Use the Javadoc @throws tag to document each unchecked exception that a method can throw,
+ * but do not use the throws keyword to include unchecked exceptions in the method declaration.
+ * *
+ * If an exception is thrown by many methods in a class for the same reason, it is
+ * acceptable to document the exception in the class's documentation comment.
+ */
+package chapter_8.item_44;
