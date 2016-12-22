710a06ffbcbf8abb7500b84c16fee39e07f64713
ildar shafigullin <ildar66@inbox.ru>
2016-12-22 11:40:01 +0300

    Item 48: Synchronize access to shared mutable data

---
+++ First_Edition/src/chapter_9/item_49/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Item 49: Avoid excessive synchronization.
+ * *
+ * To avoid the risk of deadlock, never cede control to the client within a synchronized method or block.
+ */
+package chapter_9.item_49;
