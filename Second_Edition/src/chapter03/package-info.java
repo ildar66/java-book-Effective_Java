1d7a6e2c6b82b90bdb91fffb801c91110009a024
ildar shafigullin <ildar66@inbox.ru>
2016-12-23 12:34:32 +0300

    Methods Common to All Objects.

---
+++ Second_Edition/src/chapter03/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Methods Common to All Objects.
+ * This chapter tells you when and how to override the nonfinal Object methods.
+ * The finalize method is omitted from this chapter because it was discussed in Item 7.
+ * While not an Object method, Comparable.compareTo is discussed in this chapter because it has a similar character.
+ */
+package chapter03;
