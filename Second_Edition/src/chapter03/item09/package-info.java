83812ece9a815e66188092315e98ee1b9bb30778
ildar shafigullin <ildar66@inbox.ru>
2016-12-26 12:34:22 +0300

    Item 9: Always override hashCode when you override equals. Broken - no hashCode method!

---
+++ Second_Edition/src/chapter03/item09/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Item 9: Always override hashCode when you override equals.
+ * *
+ * You must override hashCode in every class that overrides equals.
+ * *
+ * The key provision that is violated when you fail to override hashCode is the second one: equal objects must have equal hash codes.
+ */
+package chapter03.item09;
