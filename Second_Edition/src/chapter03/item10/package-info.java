183d068096e5f654a4c92b536d4639b492877528
ildar shafigullin <ildar66@inbox.ru>
2016-12-26 13:07:36 +0300

    Item 10: Always override toString.

---
+++ Second_Edition/src/chapter03/item10/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Item 10: Always override toString.
+ * *
+ * Providing a good toString implementation makes your class much more pleasant to use.
+ * see {@link chapter03.item10.PhoneNumber}
+ * *
+ * When practical, the toString method should return all of the interesting information contained in the object.
+ * *
+ * Whether or not you decide to specify the format, you should clearly document your intentions.
+ * *
+ * Provide programmatic access to all of the information contained in the value returned by toString.
+ */
+package chapter03.item10;
