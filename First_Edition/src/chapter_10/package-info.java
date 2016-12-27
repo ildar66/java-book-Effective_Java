d887137d4c2116031416c059fbf4f5dbc9ec666b
ildar shafigullin <ildar66@inbox.ru>
2016-12-27 12:35:21 +0300

    Chapter 10. Serialization.

---
+++ First_Edition/src/chapter_10/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Chapter 10. Serialization.
+ * *
+ * This chapter concerns the object serialization API, which provides a framework for encoding objects as byte streams
+ * and reconstructing objects from their byte-stream encodings.
+ * Encoding an object as a byte stream is known as serializing the object; the reverse process is known as deserializing it.
+ * Once an object has been serialized, its encoding can be transmitted from one running virtual machine to another or stored on disk
+ * for later deserialization. Serialization provides the standard wire-level object representation for remote communication,
+ * and the standard persistent data format for the JavaBeans™ component architecture.
+ */
+package chapter_10;
