7ef680c2a1d127595a9628ccd0e4d1cc19c3e51d
ildar shafigullin <ildar66@inbox.ru>
2016-12-27 12:28:58 +0300

    Item 53: Avoid thread groups.

---
+++ First_Edition/src/chapter_9/item_53/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Item 53: Avoid thread groups.
+ * *
+ * Thread groups are largely obsolete.
+ * There is a minor exception: @see {@link java.lang.ThreadGroup}.uncaughtException method.
+ */
+package chapter_9.item_53;
