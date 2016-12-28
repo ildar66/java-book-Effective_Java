c7333fe01354de016d18946456e9f07b4b374965
ildar shafigullin <ildar66@inbox.ru>
2016-12-28 13:20:40 +0300

    Classes and Interfaces

---
+++ Second_Edition/src/chapter04/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes and Interfaces.
+ * *
+ * This chapter contains guidelines to help you make the best use of these elements
+ * so that your classes and interfaces are usable, robust, and flexible.
+ */
+package chapter04;
