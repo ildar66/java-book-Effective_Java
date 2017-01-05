424e81f4145ab37427b6a575343b9434cd9dc116
ildar shafigullin <ildar66@inbox.ru>
2017-01-05 13:20:49 +0300

    Generics chapter.

---
+++ Second_Edition/src/chapter05/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Generics.
+ * *
+ * In release 1.5, generics were added to Java.
+ * Before generics, you had to cast every object you read from a collection.
+ * If someone accidentally inserted an object of the wrong type, casts could fail at runtime.
+ * With generics, you tell the compiler what types of objects are permitted in each collection.
+ * The compiler inserts casts for you automatically and tells you at compile time if you try to insert an object of the wrong type.
+ * This results in programs that are both safer and clearer, but these benefits come with complications.
+ * This chapter tells you how to maximize the benefits and minimize the complications.
+ */
+package chapter05;
