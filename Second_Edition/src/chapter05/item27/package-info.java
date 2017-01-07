ce645708e459e4fac1bcce524ff922098907260a
ildar shafigullin <ildar66@inbox.ru>
2017-01-07 13:42:55 +0300

    Item 27: Favor generic methods. Generic union method

---
+++ Second_Edition/src/chapter05/item27/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Item 27: Favor generic methods.
+ * *
+ * The type parameter list, which declares the type parameter, goes between the method’s modifiers and its return type.
+ * @see chapter05.item27.Union
+ */
+package chapter05.item27;
