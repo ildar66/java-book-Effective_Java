9ab4da971c26cea59e04715ea629ae5a086c4b4e
ildar shafigullin <ildar66@inbox.ru>
2017-01-07 14:46:57 +0300

    Item 27: Favor generic methods. Summary

--- Second_Edition/src/chapter05/item27/RecursiveTypeBound.java
+++ Second_Edition/src/chapter05/item27/RecursiveTypeBound.java
@@ -19,6 +19,18 @@ public class RecursiveTypeBound {
         return result;
     }
 
+/*    // Returns the maximum value in a list - uses recursive type bound
+    public static <T extends Comparable<? super T>> T max(List<? extends T> list) {
+        Iterator<? extends T> i = list.iterator();
+        T result = i.next();
+        while (i.hasNext()) {
+            T t = i.next();
+            if (t.compareTo(result) > 0)
+                result = t;
+        }
+        return result;
+    }*/
+
     public static void main(String[] args) {
         // List<String> argList = Arrays.asList(args);
         List<Integer> argList = Arrays.asList(1, 2, 5, 8, 7);
--- Second_Edition/src/chapter05/item27/package-info.java
+++ Second_Edition/src/chapter05/item27/package-info.java
@@ -9,5 +9,10 @@
  * Generic singleton factory pattern: {@link chapter05.item27.GenericSingletonFactory}.
  * *
  * Using a recursive type bound to express mutual comparability: {@link chapter05.item27.RecursiveTypeBound}.
+ * *
+ * In summary, generic methods, like generic types,
+ * are safer and easier to use than methods that require their clients to cast input parameters and return values.
+ * Like types, you should make sure that your new methods can be used without casts, which will often mean making them generic.
+ * And like types, you should generify your existing methods to make life easier for new users without breaking existing clients (Item 23).
  */
 package chapter05.item27;
