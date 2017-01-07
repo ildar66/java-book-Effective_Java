a8e47893c999bf8437906b745383f116935a1905
ildar shafigullin <ildar66@inbox.ru>
2017-01-07 14:13:49 +0300

    Item 27: Favor generic methods. Using a recursive type bound

--- Second_Edition/src/chapter05/item27/package-info.java
+++ Second_Edition/src/chapter05/item27/package-info.java
@@ -4,8 +4,10 @@
  * The type parameter list, which declares the type parameter, goes between the method’s modifiers and its return type.
  * @see chapter05.item27.Union
  * *
- * Generic static factory method {@link chapter05.item27.GenericStaticFactory}
+ * Generic static factory method: {@link chapter05.item27.GenericStaticFactory}.
  * *
- * Generic singleton factory pattern: {@link chapter05.item27.GenericSingletonFactory}
+ * Generic singleton factory pattern: {@link chapter05.item27.GenericSingletonFactory}.
+ * *
+ * Using a recursive type bound to express mutual comparability: {@link chapter05.item27.RecursiveTypeBound}.
  */
 package chapter05.item27;
