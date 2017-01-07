7850a2875ba7f57c79fc61e39a765f037566a386
ildar shafigullin <ildar66@inbox.ru>
2017-01-07 13:55:35 +0300

    Item 27: Favor generic methods. Generic static factory method

--- Second_Edition/src/chapter05/item27/package-info.java
+++ Second_Edition/src/chapter05/item27/package-info.java
@@ -3,5 +3,8 @@
  * *
  * The type parameter list, which declares the type parameter, goes between the method’s modifiers and its return type.
  * @see chapter05.item27.Union
+ * *
+ * Generic static factory method {@link chapter05.item27.GenericStaticFactory}
+ * *
  */
 package chapter05.item27;
