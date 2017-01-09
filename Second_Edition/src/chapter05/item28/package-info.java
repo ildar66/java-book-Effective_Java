c8b68d46a4c955e6e95eb517d0443239c91ca21e
ildar shafigullin <ildar66@inbox.ru>
2017-01-09 12:54:30 +0300

    Item 28: Use bounded wildcards to increase API flexibility. Union example. Explicit type parameter.

--- Second_Edition/src/chapter05/item28/package-info.java
+++ Second_Edition/src/chapter05/item28/package-info.java
@@ -4,5 +4,10 @@
  * For maximum flexibility, use wildcard types on input parameters that represent producers or consumers: {@link chapter05.item28.Stack}.
  * *
  * List-based generic reduction with wildcard type: @see {@link chapter05.item28.Reduction}.
+ * *
+ * Explicit type parameter:  @see {@link chapter05.item28.Union}
+ * *
+ * Do not use wildcard types as return types.
+ * If the user of a class has to think about wildcard types, there is probably something wrong with the class’s API.
  */
 package chapter05.item28;
