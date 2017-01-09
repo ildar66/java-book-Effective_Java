3302a9e06e8551f53ce46804a0e4b69ca1019db9
ildar shafigullin <ildar66@inbox.ru>
2017-01-09 12:39:35 +0300

    Item 28: Use bounded wildcards to increase API flexibility. List-based generic reduction.

--- Second_Edition/src/chapter05/item28/package-info.java
+++ Second_Edition/src/chapter05/item28/package-info.java
@@ -1,6 +1,8 @@
 /**
  * Item 28: Use bounded wildcards to increase API flexibility.
  * *
- * For maximum flexibility, use wildcard types on input parameters that represent producers or consumers: {@link chapter05.item28.Stack}
+ * For maximum flexibility, use wildcard types on input parameters that represent producers or consumers: {@link chapter05.item28.Stack}.
+ * *
+ * List-based generic reduction with wildcard type: @see {@link chapter05.item28.Reduction}.
  */
 package chapter05.item28;
