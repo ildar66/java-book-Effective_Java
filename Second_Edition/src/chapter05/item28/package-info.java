f6453d1246e8312f542bc72f90bc7ec524af6f1a
ildar shafigullin <ildar66@inbox.ru>
2017-01-09 13:12:51 +0300

    Item 28: Use bounded wildcards to increase API flexibility. Using a recursive type bound with wildcards.

--- Second_Edition/src/chapter05/item28/package-info.java
+++ Second_Edition/src/chapter05/item28/package-info.java
@@ -9,5 +9,9 @@
  * *
  * Do not use wildcard types as return types.
  * If the user of a class has to think about wildcard types, there is probably something wrong with the class’s API.
+ * *
+ * Comparables are always consumers, so you should always use Comparable<? super T> in preference to Comparable<T>.
+ * The same is true of comparators, so you should always use Comparator<? super T> in preference to Comparator<T>.
+ * Using a recursive type bound with wildcards: @see {@link chapter05.item28.RecursiveTypeBound}.
  */
 package chapter05.item28;
