dd3f0abe99300dec8c45a80d81348d0951e4d78e
ildar shafigullin <ildar66@inbox.ru>
2017-01-09 12:24:45 +0300

    Item 28: Use bounded wildcards to increase API flexibility. Stack example.

---
+++ Second_Edition/src/chapter05/item28/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Item 28: Use bounded wildcards to increase API flexibility.
+ * *
+ * For maximum flexibility, use wildcard types on input parameters that represent producers or consumers: {@link chapter05.item28.Stack}
+ */
+package chapter05.item28;
