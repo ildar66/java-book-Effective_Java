c0bd555752716290346dd08bdad3cafa3cb51172
ildar shafigullin <ildar66@inbox.ru>
2017-01-10 11:06:53 +0300

    Enums and Annotations chapter.

---
+++ Second_Edition/src/chapter06/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Enums and Annotations.
+ * *
+ * IN release 1.5, two families of reference types were added to the language:
+ * a new kind of class called an enum type, and a new kind of interface called an annotation type.
+ * This chapter discusses best practices for using these new type families.
+ */
+package chapter06;
