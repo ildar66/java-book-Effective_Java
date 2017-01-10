c9f6afcf835b020d39cf01398eec05bd926059b5
ildar shafigullin <ildar66@inbox.ru>
2017-01-10 11:44:24 +0300

    Item 30: Use enums instead of int constants.Enum type with data and behavior

---
+++ Second_Edition/src/chapter06/item30/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * Item 30: Use enums instead of int constants.
+ * *
+ * Enum types are effectively final, by virtue of having no accessible constructors.
+ * Enums provide compile-time type safety.
+ * Enum types with identically named constants coexist peacefully because each type has its own namespace.
+ * *
+ * Enum types let you add arbitrary methods and fields and implement arbitrary interfaces.
+ * They provide high-quality implementations of all the Object methods (Chapter 3),
+ * they implement Comparable (Item 12) and Serializable (Chapter 11),
+ * and their serialized form is designed to withstand most changes to the enum type.
+ * *
+ * To associate data with enum constants, {@link chapter06.item30.Planet}
+ * declare instance fields and write a constructor that takes the data and stores it in the fields.
+ */
+package chapter06.item30;
