cd51a1e894f5e4cdadd63a8adfd999f4f951fd2e
ildar shafigullin <ildar66@inbox.ru>
2017-01-11 14:30:38 +0300

    Item 33: Use EnumMap instead of ordinal indexing.

---
+++ Second_Edition/src/chapter06/item33/Phase.java
@@ -0,0 +1,62 @@
+// Using ordinal() to index array of arrays - DON'T DO THIS!
+/* public enum Phase {
+    SOLID, LIQUID, GAS;
+    public enum Transition {
+        MELT, FREEZE, BOIL, CONDENSE, SUBLIME, DEPOSIT;
+        // Rows indexed by src-ordinal, cols by dst-ordinal
+        private static final Transition[][] TRANSITIONS = {
+                { null, MELT, SUBLIME },
+                { FREEZE, null, BOIL },
+                { DEPOSIT, CONDENSE, null }
+        };
+        // Returns the phase transition from one phase to another
+        public static Transition from(Phase src, Phase dst) {
+            return TRANSITIONS[src.ordinal()][dst.ordinal()];
+        }
+    }
+}*/
+
+// Using a nested EnumMap to associate data with enum pairs - Pags 163-164
+package chapter06.item33;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public enum Phase {
+    SOLID, LIQUID, GAS, PLASMA;
+
+    public enum Transition {
+        MELT(SOLID, LIQUID), FREEZE(LIQUID, SOLID), BOIL(LIQUID, GAS), CONDENSE(GAS, LIQUID),
+        SUBLIME(SOLID, GAS), DEPOSIT(GAS, SOLID), IONIZE(GAS, PLASMA), DEIONIZE(PLASMA, GAS);
+
+        private final Phase src;
+        private final Phase dst;
+
+        Transition(Phase src, Phase dst) {
+            this.src = src;
+            this.dst = dst;
+        }
+
+        // Initialize the phase transition map
+        private static final Map<Phase, Map<Phase, Transition>> m = new EnumMap<Phase, Map<Phase, Transition>>(Phase.class);
+
+        static {
+            for (Phase p : Phase.values())
+                m.put(p, new EnumMap<Phase, Transition>(Phase.class));
+            for (Transition trans : Transition.values())
+                m.get(trans.src).put(trans.dst, trans);
+        }
+
+        public static Transition from(Phase src, Phase dst) {
+            return m.get(src).get(dst);
+        }
+    }
+
+    // Simple demo program - prints a sloppy table
+    public static void main(String[] args) {
+        for (Phase src : Phase.values())
+            for (Phase dst : Phase.values())
+                if (src != dst)
+                    System.out.printf("%s to %s : %s %n", src, dst, Transition.from(src, dst));
+    }
+}
--- Second_Edition/src/chapter06/item33/package-info.java
+++ Second_Edition/src/chapter06/item33/package-info.java
@@ -1,6 +1,12 @@
 /**
  * Item 33: Use EnumMap instead of ordinal indexing.
  * *
- * Using an EnumMap to associate data with an enum: {@link chapter06.item33.Herb}
+ * Using an EnumMap to associate data with an enum: {@link chapter06.item33.Herb}.
+ * *
+ * Using a nested EnumMap to associate data with enum pairs: {@link chapter06.item33.Phase}.
+ * *
+ * In summary, it is rarely appropriate to use ordinals to index arrays: use EnumMap instead.
+ * If the relationship that you are representing is multidimensional, use EnumMap<..., EnumMap<...>>.
+ * This is a special case of the general principle that application programmers should rarely, if ever, use Enum.ordinal (Item 31).
  */
 package chapter06.item33;
