ece833f106dba4eb08bb598927ffac252b15006d
ildar shafigullin <ildar66@inbox.ru>
2017-01-12 14:00:08 +0300

    Item 35: Prefer annotations to naming patterns.Annotation type with an array parameter.

--- Second_Edition/src/chapter06/item35/package-info.java
+++ Second_Edition/src/chapter06/item35/package-info.java
@@ -4,5 +4,9 @@
  * Marker annotation type declaration: {@link chapter06.item35.Test}.
  * Program containing marker annotations: {@link chapter06.item35.Sample}.
  * Program to process marker annotations: {@link chapter06.item35.RunTests}
+ * *
+ * Annotation type with an array parameter: {@link chapter06.item35.ExceptionTest}.
+ * Program containing annotations with a parameter: {@link chapter06.item35.Sample2}.
+ * Program to process marker annotations with a parameter: {@link chapter06.item35.RunTests2}
  */
 package chapter06.item35;
