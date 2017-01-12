ddd0c5129a8f3d8bcfb3f2b5dde2a86d41c3afb9
ildar shafigullin <ildar66@inbox.ru>
2017-01-12 13:37:28 +0300

    Item 35: Prefer annotations to naming patterns.Marker annotation type declaration, usage and run.

---
+++ Second_Edition/src/chapter06/item35/RunTests.java
@@ -0,0 +1,29 @@
+// Program to process marker annotations - Page 171
+package chapter06.item35;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class RunTests {
+
+    public static void main(String[] args) throws Exception {
+        int tests = 0;
+        int passed = 0;
+        Class testClass = Sample.class; //Class.forName(args[0]);
+        for (Method m : testClass.getDeclaredMethods()) {
+            if (m.isAnnotationPresent(Test.class)) {
+                tests++;
+                try {
+                    m.invoke(null);
+                    passed++;
+                } catch (InvocationTargetException wrappedExc) {
+                    Throwable exc = wrappedExc.getCause();
+                    System.out.println(m + " failed: " + exc);
+                } catch (Exception exc) {
+                    System.out.println("INVALID @Test: " + m);
+                }
+            }
+        }
+        System.out.printf("Passed: %d, Failed: %d%n", passed, tests - passed);
+    }
+}
---
+++ Second_Edition/src/chapter06/item35/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Item 35: Prefer annotations to naming patterns.
+ * *
+ * Marker annotation type declaration: {@link chapter06.item35.Test}.
+ * Program containing marker annotations: {@link chapter06.item35.Sample}.
+ * Program to process marker annotations: {@link chapter06.item35.RunTests}
+ */
+package chapter06.item35;
