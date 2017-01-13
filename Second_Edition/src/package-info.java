8d3b5c6a05063e18b7be7a2844cbf17a75725105
ildar shafigullin <ildar66@inbox.ru>
2017-01-13 13:31:58 +0300

    src info.

--- Second_Edition/src/package-info.java
+++ Second_Edition/src/package-info.java
@@ -1,8 +1,8 @@
-/*
+/**
  * # java-book-Effective_Java
 Effective Java(customary and effective usage):
-    1 Introduction (chapter01).
-    2 Creating and Destroying Objects (chapter02):
+    1 Introduction ({@link chapter01}).
+    2 Creating and Destroying Objects ({@link chapter02}):
         Item 1: Consider static factory methods instead of constructors.
         Item 2: Consider a builder when faced with many constructor parameters.
         Item 3: Enforce the singleton property with a private constructor or an enum type.
@@ -10,13 +10,13 @@ Effective Java(customary and effective usage):
         Item 5: Avoid creating unnecessary objects.
         Item 6: Eliminate obsolete object references.
         Item 7: Avoid finalizers.
-    3 Methods Common to All Objects (chapter03):
+    3 Methods Common to All Objects ({@link chapter03}):
         Item 8: Obey the general contract when overriding equals.
         Item 9: Always override hashCode when you override equals.
         Item 10: Always override toString.
         Item 11: Override clone judiciously.
         Item 12: Consider implementing Comparable.
-    4 Classes and Interfaces (chapter04):
+    4 Classes and Interfaces ({@link chapter04}):
         Item 13: Minimize the accessibility of classes and members.
         Item 14: In public classes, use accessor methods, not public fields.
         Item 15: Minimize mutability.
@@ -27,7 +27,7 @@ Effective Java(customary and effective usage):
         Item 20: Prefer class hierarchies to tagged classes.
         Item 21: Use function objects to represent strategies.
         Item 22: Favor static member classes over nonstatic.
-    5 Generics (chapter05):
+    5 Generics ({@link chapter05}):
         Item 23: Don’t use raw types in new code.
         Item 24: Eliminate unchecked warnings.
         Item 25: Prefer lists to arrays.
@@ -35,7 +35,7 @@ Effective Java(customary and effective usage):
         Item 27: Favor generic methods.
         Item 28: Use bounded wildcards to increase API flexibility.
         Item 29: Consider typesafe heterogeneous containers.
-    6 Enums and Annotations (chapter06):
+    6 Enums and Annotations ({@link chapter06}):
         Item 30: Use enums instead of int constants.
         Item 31: Use instance fields instead of ordinals.
         Item 32: Use EnumSet instead of bit fields.
