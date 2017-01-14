3f3a58a8067f17a02d712506deb38af959041329
ildar shafigullin <ildar66@inbox.ru>
2017-01-14 13:49:06 +0300

    Item 39: Make defensive copies when needed. Broken "immutable" time period.

---
+++ Second_Edition/src/chapter07/item39/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Item 39: Make defensive copies when needed.
+ * *
+ * You must program defensively, with the assumption that clients of your class will do their best to destroy its invariants.
+ * *
+ * Broken "immutable" time period class: {@link chapter07.item39.Period}.
+ * @see chapter07.item39.Attack the internals of a Period instance.
+ */
+package chapter07.item39;
