a43ccb42259a655fb2f8ce36cfd915acf8fe30dd
ildar shafigullin <ildar66@inbox.ru>
2017-01-18 12:44:25 +0300

    General Programming chapter.

---
+++ Second_Edition/src/chapter08/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * General Programming.
+ * *
+ * THIS chapter is largely devoted to the nuts and bolts of the language.
+ * It discusses the treatment of local variables, control structures, the use of libraries, the use of various data types,
+ * and the use of two extralinguistic facilities: reflection and native methods.
+ * Finally, it discusses optimization and naming conventions.
+ */
+package chapter08;
