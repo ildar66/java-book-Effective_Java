ff9c7c3fbacf2362ec6a89bef362ec487e600e4d
ildar shafigullin <ildar66@inbox.ru>
2017-01-19 12:57:37 +0300

    Item 47: Know and use the libraries.

---
+++ Second_Edition/src/chapter08/item47/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * Item 47: Know and use the libraries.
+ * *
+ * By using a standard library, you take advantage of the knowledge of the experts
+ * who wrote it and the experience of those who used it before you.
+ * *
+ * Numerous features are added to the libraries in every major release, and it pays to keep abreast of these additions.
+ * Every programmer should be familiar with the contents of java.lang, java.util, and, to a lesser extent, java.io.
+ * *
+ * To summarize, don’t reinvent the wheel. If you need to do something that seems like it should be reasonably common,
+ * there may already be a class in the libraries that does what you want. If there is, use it; if you don’t know, check.
+ * Generally speaking, library code is likely to be better than code that you’d write yourself and is likely to improve over time.
+ * This is no reflection on your abilities as a programmer.
+ * Economies of scale dictate that library code receives far more attention than most developers could afford to devote to the same functionality.
+ */
+package chapter08.item47;
