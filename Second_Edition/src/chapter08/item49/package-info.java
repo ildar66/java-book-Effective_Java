eed17ef84d514e9eabbe36a606d7e52fb2f4f0fb
ildar shafigullin <ildar66@inbox.ru>
2017-01-20 10:57:49 +0300

    Item 49: Prefer primitive types to boxed primitives.Fixed Comparator

--- Second_Edition/src/chapter08/item49/package-info.java
+++ Second_Edition/src/chapter08/item49/package-info.java
@@ -1,6 +1,9 @@
 /**
  * Item 49: Prefer primitive types to boxed primitives.
  * *
- * Broken comparator: {@link chapter08.item49.BrokenComparator}
+ * Broken comparator: {@link chapter08.item49.BrokenComparator}.
+ * Applying the == operator to boxed primitives is almost always wrong.
+ * Fixed Comparator(commented).
+ * *
  */
 package chapter08.item49;
