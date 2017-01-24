61ea8ad037194c8f6d28f5e565c12f87f388bd52
ildar shafigullin <ildar66@inbox.ru>
2017-01-24 11:35:35 +0300

    Item 56: Adhere to generally accepted naming conventions.

---
+++ Second_Edition/src/chapter08/item56/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * Item 56: Adhere to generally accepted naming conventions.
+ * *
+ * The Java platform has a well-established set of naming conventions,
+ * many of which are contained in The Java Language Specification [JLS, 6.8].
+ * *
+ * For quick reference, the following table shows examples of typographical conventions:
+ * Identifier:                       |             Type Examples:
+ * Package                           |      com.google.inject, org.joda.time.format
+ * Class or Interface                |      Timer, FutureTask, LinkedHashMap, HttpServlet
+ * Method or Field                   |      remove, ensureCapacity, getCrc
+ * Constant Field                    |      MIN_VALUE, NEGATIVE_INFINITY
+ * Local Variable                    |      i, xref, houseNumber
+ * Type Parameter                    |      T, E, K, V, X, T1, T2
+ * *
+ * To summarize, internalize the standard naming conventions and learn to use them as second nature.
+ * The typographical conventions are straightforward and largely unambiguous;
+ * the grammatical conventions are more complex and looser.
+ * To quote from The Java Language Specification [JLS, 6.8],
+ * “These conventions should not be followed slavishly if long-held conventional usage dictates otherwise.” Use common sense.
+ */
+package chapter08.item56;
