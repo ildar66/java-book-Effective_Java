21513c589750c97f4b23daef26ea371a341acd17
ildar shafigullin <ildar66@inbox.ru>
2017-01-24 12:00:33 +0300

    Exception chapter.

---
+++ Second_Edition/src/chapter09/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Exceptions.
+ * *
+ * When used to best advantage, exceptions can improve a program’s readability, reliability, and maintainability.
+ * When used improperly, they can have the opposite effect.
+ * This chapter provides guidelines for using exceptions effectively
+ */
+package chapter09;
