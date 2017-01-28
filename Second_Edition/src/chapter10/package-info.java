cd4cf67e70ec06aff23287b8ede0b08555dbbd27
ildar shafigullin <ildar66@inbox.ru>
2017-01-28 10:28:13 +0300

    Concurrency chapter.

---
+++ Second_Edition/src/chapter10/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Concurrency.
+ * *
+ * THREADS allow multiple activities to proceed concurrently.
+ * Concurrent programming is harder than single-threaded programming,
+ * because more things can go wrong, and failures can be hard to reproduce.
+ * But you can’t avoid concurrency. It is inherent in much of what we do, and a requirement
+ * if you are to obtain good performance from multicore processors, which are now commonplace.
+ * *
+ * This chapter contains advice to help you write clear, correct, well-documented concurrent programs.
+ */
+package chapter10;
