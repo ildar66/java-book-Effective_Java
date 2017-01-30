72ec063dc927f4fa12d1d454dc5804a8bcc1b158
ildar shafigullin <ildar66@inbox.ru>
2017-01-30 08:31:17 +0300

    Item 66: Synchronize access to shared mutable data.Cooperative thread termination with a volatile field

--- Second_Edition/src/chapter10/item66/package-info.java
+++ Second_Edition/src/chapter10/item66/package-info.java
@@ -7,6 +7,10 @@
  * *
  * Broken {@link chapter10.item66.brokenstopthread.StopThread}
  * *
- * One way to fix the problem is to synchronize access to the stopRequested field: {@link chapter10.item66.fixedstopthread1.StopThread}
+ * One way to fix the problem is to synchronize access to the stopRequested field: {@link chapter10.item66.fixedstopthread1.StopThread}.
+ * Synchronization has no effect unless both read and write operations are synchronized.
+ * *
+ * The locking in the second version of StopThread can be omitted
+ * if stopRequested is declared volatile: {@link chapter10.item66.fixedstopthread2.StopThread}
  */
 package chapter10.item66;
