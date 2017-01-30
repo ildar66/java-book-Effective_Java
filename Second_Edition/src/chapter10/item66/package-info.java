a0b29a918093dc2434985045733d743706d8df56
ildar shafigullin <ildar66@inbox.ru>
2017-01-30 08:16:16 +0300

    Item 66: Synchronize access to shared mutable data. Broken StopThread

---
+++ Second_Edition/src/chapter10/item66/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Item 66: Synchronize access to shared mutable data.
+ * *
+ * Synchronization is required for reliable communication between threads as well as for mutual exclusion.
+ * *
+ * Do not use Thread.stop.
+ * *
+ * Broken {@link chapter10.item66.brokenstopthread.StopThread}
+ */
+package chapter10.item66;
