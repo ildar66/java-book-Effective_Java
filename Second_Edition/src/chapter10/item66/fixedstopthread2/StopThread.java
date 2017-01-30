ac9a5ea732fb8f0d977db59e1697d40f77929733
ildar shafigullin <ildar66@inbox.ru>
2017-01-30 08:42:07 +0300

    Item 66: Synchronize access to shared mutable data. You do have to be careful when using volatile.

--- Second_Edition/src/chapter10/item66/fixedstopthread2/StopThread.java
+++ Second_Edition/src/chapter10/item66/fixedstopthread2/StopThread.java
@@ -22,3 +22,18 @@ public class StopThread {
         stopRequested = true;
     }
 }
+
+    /* You do have to be careful when using volatile.
+       Consider the following method,which is supposed to generate serial numbers:
+       (The problem is that the increment operator (++) is not atomic.)
+    // Broken - requires synchronization!
+        private static volatile int nextSerialNumber=0;
+        public static int generateSerialNumber(){
+            return nextSerialNumber++;
+    }*/
+
+    /* Better still, follow the advice in Item 47 and use the class AtomicLong, which is part of java.util.concurrent.atomic.
+    private static final AtomicLong nextSerialNum = new AtomicLong();
+    public static long generateSerialNumber() {
+        return nextSerialNum.getAndIncrement();
+    }*/
