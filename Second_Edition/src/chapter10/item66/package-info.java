acdd68f255174361081011d175509aef34efda2a
ildar shafigullin <ildar66@inbox.ru>
2017-01-30 08:56:30 +0300

    Item 66: Synchronize access to shared mutable data.Summary.

--- Second_Edition/src/chapter10/item66/package-info.java
+++ Second_Edition/src/chapter10/item66/package-info.java
@@ -1,7 +1,8 @@
 /**
  * Item 66: Synchronize access to shared mutable data.
  * *
- * Synchronization is required for reliable communication between threads as well as for mutual exclusion.
+ * Synchronization is required for reliable communication between threads as well as for mutual exclusion
+ * (to prevent an object from being observed in an inconsistent state while it’s being modified by another thread).
  * *
  * Do not use Thread.stop.
  * *
@@ -11,6 +12,17 @@
  * Synchronization has no effect unless both read and write operations are synchronized.
  * *
  * The locking in the second version of StopThread can be omitted
- * if stopRequested is declared volatile: {@link chapter10.item66.fixedstopthread2.StopThread}
+ * if stopRequested is declared volatile: {@link chapter10.item66.fixedstopthread2.StopThread}.
+ * *
+ * Either share immutable data (Item 15), or don’t share at all.
+ * In other words, confine mutable data to a single thread.
+ * *
+ * In summary, when multiple threads share mutable data, each thread that reads or writes the data must perform synchronization.
+ * Without synchronization, there is no guarantee that one thread’s changes will be visible to another.
+ * The penalties for failing to synchronize shared mutable data are liveness and safety failures.
+ * These failures are among the most difficult to debug.
+ * They can be intermittent and timing-dependent, and program behavior can vary radically from one VM to another.
+ * If you need only inter-thread communication, and not mutual exclusion,
+ * the volatile modifier is an acceptable form of synchronization, but it can be tricky to use correctly.
  */
 package chapter10.item66;
