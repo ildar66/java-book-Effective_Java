e0d2235ce039d14f130f14842707cafe03e88616
ildar shafigullin <ildar66@inbox.ru>
2017-01-31 13:44:54 +0300

    Item 69: Prefer concurrency utilities to wait and notify. ConcurrentMap String.intern simulation.

---
+++ Second_Edition/src/chapter10/item69/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Item 69: Prefer concurrency utilities to wait and notify.
+ * *
+ * Given the difficulty of using wait and notify correctly, you should use the higher-level concurrency utilities instead.
+ * *
+ * For example, this {@link chapter10.item69.Intern} simulates the behavior of String.intern.
+ */
+package chapter10.item69;
