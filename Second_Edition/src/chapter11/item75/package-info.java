b09f903479bf9553fd09edd72713a9cf7bb69c4a
ildar shafigullin <ildar66@inbox.ru>
2017-02-03 12:50:32 +0300

    Item 75: Consider using a custom serialized form. StringList with a reasonable custom serialized form

--- Second_Edition/src/chapter11/item75/package-info.java
+++ Second_Edition/src/chapter11/item75/package-info.java
@@ -9,5 +9,13 @@
  * Even if you decide that the default serialized form is appropriate,
  * you often must provide a readObject method to ensure invariants and security.
  * In the case of {@link chapter11.item75.Name}, the readObject method must ensure that lastName and firstName are non-null (Items 76 and 78).
+ * *
+ * Using the default serialized form when an object’s physical representation differs substantially from its logical data content
+ * has four disadvantages:
+ * • It permanently ties the exported API to the current internal representation.
+ * • It can consume excessive space.
+ * • It can consume excessive time.
+ * • It can cause stack overflows.
+ * @see chapter11.item75.StringList with a reasonable custom serialized form.
  */
 package chapter11.item75;
