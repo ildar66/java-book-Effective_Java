8f6c3f84c2f4632d73af14b5a459b7686406e5dc
ildar shafigullin <ildar66@inbox.ru>
2017-02-03 13:07:21 +0300

    Item 75: Consider using a custom serialized form. Summary.

--- Second_Edition/src/chapter11/item75/package-info.java
+++ Second_Edition/src/chapter11/item75/package-info.java
@@ -17,5 +17,29 @@
  * • It can consume excessive time.
  * • It can cause stack overflows.
  * @see chapter11.item75.StringList with a reasonable custom serialized form.
+ * *
+ * If all instance fields are transient, it is technically permissible to dispense with invoking defaultWriteObject and defaultReadObject,
+ * but it is not recommended.
+ * *
+ * Before deciding to make a field nontransient, convince yourself that its value is part of the logical state of the object.
+ * *
+ * You must impose any synchronization on object serialization that you would impose on any other method
+ * that reads the entire state of the object.
+ * // writeObject for synchronized class with default serialized form:
+ * private synchronized void writeObject(ObjectOutputStream s) throws IOException {s.defaultWriteObject();}
+ * *
+ * Regardless of what serialized form you choose, declare an explicit serial version UID in every serializable class you write.
+ * // Declaring a serial version UID is simple. Just add this line to your class:
+ * private static final long serialVersionUID = "randomLongValue" ;
+ * If you write a new class, it doesn’t matter what value you choose for "randomLongValue".
+ * *
+ * To summarize,
+ * when you have decided that a class should be serializable(Item 74), think hard about what the serialized form should be.
+ * Use the default serialized form only if it is a reasonable description of the logical state of the object;
+ * otherwise design a custom serialized form that aptly describes the object.
+ * You should allocate as much time to designing the serialized form of a class as you allocate to designing its exported methods (Item 40).
+ * Just as you cannot eliminate exported methods from future versions, you cannot eliminate fields from the serialized form;
+ * they must be preserved forever to ensure serialization compatibility.
+ * Choosing the wrong serialized form can have a permanent, negative impact on the complexity and performance of a class.
  */
 package chapter11.item75;
