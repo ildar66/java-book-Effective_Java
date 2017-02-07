36dd750e201f896e4671da91a4d84d47e92fc2d8
ildar shafigullin <ildar66@inbox.ru>
2017-02-07 09:58:43 +0300

    Item 78: Consider serialization proxies instead of serialized instances.Period class with serialization proxy

---
+++ Second_Edition/src/chapter11/item78/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Item 78: Consider serialization proxies instead of serialized instances.
+ * *
+ * {@link chapter11.item78.Period} class with serialization proxy
+ */
+package chapter11.item78;
